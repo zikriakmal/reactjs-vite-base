8898fdfcd0fa8863added2fea438491ba82b470f
Zikri Akmal S <zikriakmale@gmail.com>
2024-02-14 20:10:30 +0700

    feat: add tailwind css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,11 +3,10 @@ import { Link } from "react-router-dom";
 const Home = () => {
     return (
         <div>
-            <p>test</p>
+            <p  className="text-3xl">test</p>
             <Link to={"/contact"}>coba ke contact donkse</Link>
         </div>
     )
 }
-
 
 export default Home;
