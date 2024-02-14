0262eb5d615ead067d23e33eb45dcaa5bd612498
Zikri Akmal S <zikriakmale@gmail.com>
2024-02-14 20:23:46 +0700

    feat: combine react router dom + tailwind + antdesign into home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,44 @@
+import { Button, Dropdown, MenuProps, Space } from "antd";
 import { Link } from "react-router-dom";
+
+const items: MenuProps['items'] = [
+    {
+        label: <a href="https://www.antgroup.com">1st menu item</a>,
+        key: '0',
+    },
+    {
+        label: <a href="https://www.aliyun.com">2nd menu item</a>,
+        key: '1',
+    },
+    {
+        type: 'divider',
+    },
+    {
+        label: '3rd menu item',
+        key: '3',
+    },
+];
 
 const Home = () => {
     return (
-        <div>
-            <p  className="text-3xl">test</p>
-            <Link to={"/contact"}>coba ke contact donkse</Link>
+        <div className="flex flex-col gap-8">
+            <p className="text-3xl">This is vite + react js + react router dom + tailwind + ant design</p>
+            <div>
+                <Link to={"/contact"}>This is router to contact page</Link>
+            </div>
+            <div>
+                <Button className="bg-slate-700" type="primary">This is ant design button example</Button>
+            </div>
+            <div className="border rounded-md self-center px-4 py-2 bg-slate-700">
+                <Dropdown menu={{ items }} trigger={['click']}>
+                    <a onClick={(e) => e.preventDefault()} className="text-blue-50">
+                        <Space>
+                            This is ant design menu dropdown
+                        </Space>
+                    </a>
+                </Dropdown>
+
+            </div>
         </div>
     )
 }
